ad2f97d3d8a1835f73adce4ba47922c508d3fc6e
its-mr-monday <zackary.live8@gmail.com>
2024-08-01 04:42:05 +0000

    some update to setup for buil env,  currently Debian is supported

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+directoryx="$(dirname -- $(readlink -fn -- "$0"; echo x))"
+BASEDIR="${directoryx%x}"
+STARTDIR=$PWD
+
 #Most distrobutions of Linux require root/sudo privs to install packages
 root_checker() {
     if [[ $EUID != 0 ]]; then
@@ -16,12 +20,23 @@ linux_check() {
     fi
 }
 
-linux_check
+create_dirs() {
+    mkdir /etc/lightbalancer
+    mkdir /var/log/lightbalancer
+}
+
+create_resource_files() {
+    cd $BASEDIR
+    cp  ../samples/default.json /etc/lightbalancer/config.json
+    touch /var/log/lightbalancer/info.log
+    touch /var/log/lightbalancer/error.log
+}
 
 # As we support more flavors of Linux, we will add more package managers
 # to this list.
 debian_install() {
     root_checker()
+    echo "[*] Installing dependencies for Debian-based Linux"
     apt update
     apt install -y gcc g++ make cmake openssl python3-dev libssl-dev libffi-dev zlib1g pkg-config
     echo "[*] Dependencies installed for Ubuntu!"
@@ -36,3 +51,14 @@ dependecy_install() {
         exit 1
     fi
 }
+
+main() {
+    linux_check
+    dependency_install
+    create_dirs
+    create_resource_files
+    echo "[!] Setup development environment for LightBalancer"
+    exit 0
+}
+
+main
